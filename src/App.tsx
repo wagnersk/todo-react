5fa723de1c8e733f13e7c89de653885d72351cb7
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-10-22 09:46:01 +0100

    feature: card

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
+import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
 
 export default function App() {
   return (
@@ -50,6 +52,14 @@ export default function App() {
 
       <div>
         <InputText />
+      </div>
+
+      <div>
+        <InputCheckbox />
+      </div>
+
+      <div>
+        <Card size="md">Olá mundo</Card>
       </div>
     </div>
   );
