1328f40bac77c446d1d0fe3c2d97ee5bb3fa801d
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-10-22 08:29:08 +0100

    feature: Text - icons - badge components

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import Text from "./components/text";
+
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import Icon from "./components/icon";
+import Badge from "./components/badge";
+
+export default function App() {
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text className="text-green-base">Olá mundo!</Text>
+        <Text variant="body-md-bold">Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+    </div>
+  );
+}
