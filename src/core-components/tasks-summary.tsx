582cb0e1c87de7518d526f48576caa8c5d0c3613
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-11-06 21:52:44 +0000

    FEATURE: Atualizando badges do resumos das tarefas

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,17 +1,27 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
+    const { createdTasksCount, concludedTasksCount } = useTasks();
+
     return (
         <>
             <div className="flex items-center gap-2">
-                <Text variant="body-md-bold" className="!text-gray-300">Tarefas criadas</Text>
-                <Badge variant="secondary">5</Badge>
+                <Text className="!text-gray-300" variant="body-sm-bold">
+                    Tarefas criadas
+                </Text>
+                <Badge variant="secondary">{createdTasksCount}</Badge>
             </div>
+
             <div className="flex items-center gap-2">
-                <Text variant="body-md-bold" className="!text-gray-300"> Concluidas</Text>
-                <Badge variant="primary">2 de 5</Badge>
+                <Text className="!text-gray-300" variant="body-sm-bold">
+                    Concluídas
+                </Text>
+                <Badge variant="primary">
+                    {concludedTasksCount} de {createdTasksCount}
+                </Badge>
             </div>
         </>
-    )
+    );
 }
